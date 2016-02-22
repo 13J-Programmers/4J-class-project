b24f226bb71f61e791d85f3e4883475d893a4605
Mako <mnfeconicu41@gmail.com>
2016-02-22 16:57:05 +0900

    remove node app (`rm -rf node_modules/` to remove completely)

--- public/javascripts-es6/main.es6
+++
@@ -1,9 +0,0 @@
-
-const mainGame = game.Game.instance;
-const gameScene = new game.GameScene();
-
-new game.Player(gameScene);
-new game.OrbitControls(gameScene);
-
-mainGame.setScene(gameScene);
-mainGame.start();
--- routes/game.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET game. */
-router.get('/', (req, res, next) => {
-  res.render('game', { title: '4J Class Project' });
-});
-
-module.exports = router;
